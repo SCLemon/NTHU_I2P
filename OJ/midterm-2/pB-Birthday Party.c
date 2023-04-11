941babc30173b4fccfab62eb49b79709f50a659a
SC1314520 <blc0000421@gmail.com>
2023-04-11 21:34:01 +0800

    新增註解

--- OJ/midterm-2/pB-Birthday Party.c
+++ OJ/midterm-2/pB-Birthday Party.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-
+// It is the smallest positive integer that is a multiple of two or more integers.
+// a * b = gcd(a, b) * lcm(a, b)
 #define lcm(a, b) (a / gcd(a, b) * b)
 
 long long gcd(long long n, long long k);
@@ -13,11 +14,18 @@ int main() {
         R = gcd(n, r);
         B = gcd(n, b);
         Y = gcd(n, y);
+        // the smallest positive integer of R,B,Y
         d = lcm(lcm(R, B), Y);
         printf("%lld\n", n / d);
     }
 }
 // 最大公因數
+/*
+ We do this because the problem requires that no two adjacent balls have the same color.
+ So, we want to find the largest factor that divides n and each color,
+ because that will give us the maximum number of balls that can be colored with that color
+ without violating the condition.
+ */
 long long gcd(long long n, long long k) {
     while (k) { // k!=0
         long long tmp = n % k;
