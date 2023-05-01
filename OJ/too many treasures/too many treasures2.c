92cb35f94953f8248e78d9825a199d2dd0e128bf
SC1314520 <g0710126@hchs.hc.edu.tw>
2023-05-01 16:44:47 +0800

    update

--- OJ/too many treasures/too many treasures2.c
+++ OJ/too many treasures/too many treasures2.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
-long long int prefix[1000005];
+long long int arr[1000005];
 int main(){
     int n,q,l,r,m;
-    scanf("%d %d",&n,&q);
+    scanf("%d%d",&n,&q);
     int x;
     for(int i=1;i<=n;i++){
         scanf("%d",&x);
-        if(x<0)x=0;
-        prefix[i]=prefix[i-1]+x;
+        if(x<0) x=0;
+        arr[i]=arr[i-1]+x;
     }
-    for(int i=0;i<q;i++){
+    for(int j=0;j<q;j++){
         scanf("%d%d%d",&l,&r,&m);
-        long long int answer=prefix[l+m-1]-prefix[l-1];
-        printf("%lld\n",answer);
+        printf("%lld\n",(arr[l+m-1]-arr[l-1]));
     }
     return 0;
 }
